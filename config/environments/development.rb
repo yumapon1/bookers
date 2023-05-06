7e6cff34227e5c59c3cebe5fa577088f048ca9ce
YumaTagami <tagami.yuma.infratop@gmail.com>
2023-05-06 13:34:21 +0000

    [Add]ホストの権限

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "7fc68348988a43dcbbfb7e1c25cc3b21.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
