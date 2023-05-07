7260614809da73aad12ce77a39ffc846b91fceeb
YumaTagami <tagami.yuma.infratop@gmail.com>
2023-05-07 15:39:18 +0000

    中断

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,42 @@
+class BooksController < ApplicationController
+  def new
+     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @book =Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def index
+    @books=Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+ def update
+   book = Book.find(params[:id])
+   book.update(book_params)
+   redirect_to book_path(book.id)
+ end
+
+ def destroy
+  book= Book.find(params[:id])
+  book.destroy
+  redirect_to '/books'
+ end
+
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
